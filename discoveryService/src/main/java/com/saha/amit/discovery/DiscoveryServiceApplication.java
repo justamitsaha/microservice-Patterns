f46dc87dafea94685e26960736a2280085b47183
Amit Saha <justamitsaha@gmail.com>
2025-10-19 17:19:01 +0530

    configuration set up

--- discoveryService/src/main/java/com/saha/amit/discovery/DiscoveryServiceApplication.java
+++ discoveryService/src/main/java/com/saha/amit/discovery/DiscoveryServiceApplication.java
@@ -1,5 +1,7 @@
 package com.saha.amit.discovery;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -7,7 +9,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServiceApplication {
+    private static final Logger logger = LoggerFactory.getLogger(DiscoveryServiceApplication.class);
     public static void main(String[] args) {
+        logger.info("http://localhost:8761/");
         SpringApplication.run(DiscoveryServiceApplication.class, args);
     }
 }
