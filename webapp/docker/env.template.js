08f3c181622e7b4288f734b202e707a1b7127f0d
Amit Saha <justamitsaha@gmail.com>
2025-11-06 17:56:08 +0530

    Dockerize Angular

--- webapp/docker/env.template.js
+++ webapp/docker/env.template.js
@@ -1,12 +1,11 @@
 (function (window) {
     window.__env = window.__env || {};
-    window.__env.apiBaseUrl = "http://localhost:8085";
-    window.__env.appThemeColor = "orange";
+    window.__env.apiBaseUrl = "$API_BASE_URL";
+    window.__env.appThemeColor = "$APP_THEME_COLOR";
 
     // ✅ Apply theme color to CSS variable
     document.documentElement.style.setProperty(
         "--app-theme-color",
         window.__env.appThemeColor
     );
-
 })(this);
